5f996dd3f6fd9339aa5bc8a4eeeabdfbc58d5c05
Salvador Diaz <163175917+slvDiaz@users.noreply.github.com>
2024-11-27 01:17:03 -0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,8 @@
+function hoverCat() {
+    var video = document.getElementById('video-cat');
+    video.play();
+}
+function outCat() {
+    var video = document.getElementById('video-cat');
+    video.pause();
+}
